18238df22a5fc0d740bde304f6847afdac2f4cd7
Elizabeth <laliroal@gmail.com>
2013-10-01 14:13:06 -0300

    Preparando archivos para entrega

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,24 +1,44 @@
-require 'clase_producto.rb'
+# encoding: utf-8
+class Product
+  attr_accessor :nombre, :precio, :codigo
+  def initialize (c, n, p)
+    @nombre=n
+    @precio=p
+    @codigo=c
+  end
+  def nombre
+    puts @nombre
+  end
+  def precio
+    puts @precio
+  end
+  def codigo
+    puts @codigo
+  end
+end
 
 class Purchase 
-	def initialize
-		products= Array.new
-	end
-	
-	def add p
-		product.push(p)
-	end
-	def total
-		t= products.inject {0} {|producto, n | producto.precio + n }
-		puts t
-	end
-	def delete p
-		 products.delete_if {|produc| produc.codigo = p.codigo} 
-	end
-	def apply_discount
-	t= total
-	if products.length > 10 { 
-		t= t - t*20/100	}
-	puts t	
-	end
+  def initialize
+    @products= []
+    @total=0
+  end
+  def add p
+    products << p
+    @total += p.precio
+  end
+  def total
+    puts #{@total}
+  end
+  def delete p
+    products.delete(p)
+    @total -= p.precio
+  end
 end
+p1 = Product.new '01', 'Martillo', '50'
+p2 = Product.new '02', 'Clavos x 100', '10'
+p = Purchase.new
+p.add p1
+p.add p2
+
+p.total
+
